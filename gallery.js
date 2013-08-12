a6d7106b41fa8b9d0c1e5affa0a2094953555d8d
Luchs <Luchs@euirc.eu>
2013-08-12 20:27:29 +0200

    Make big image view modal

--- gallery.js
+++ gallery.js
@@ -6,13 +6,18 @@ define(['Ractive', 'rv!./template'], function(Ractive, template) {
     init: function(options) {
       // Default values
       this.set('selected', 0);
+      this.set('bigview', false);
 
       this.on({
-	select: function(event, item) {
-	  item = Math.max(0, item);
-	  item = Math.min(this.get('images').length - 1, item);
-	  this.set('selected', item);
-	}
+        select: function(event, item) {
+          item = Math.max(0, item);
+          item = Math.min(this.get('images').length - 1, item);
+          this.set('selected', item);
+          this.set('bigview', true);
+        },
+        close: function() {
+          this.set('bigview', false);
+        }
       });
     }
   });
